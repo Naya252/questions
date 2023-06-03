e21014bcfa771de120556c7b73e227db598e20b8
Naya <naya252naya@gmail.com>
2023-06-03 15:03:31 +0300

    add client

---
+++ client/src/layouts/AdminLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="admin-layout-wrap">
+    <el-container :style="{ height: '100%' }">
+      <!-- <el-aside width="250px"> -->
+      <admin-menu />
+      <!-- </el-aside> -->
+      <el-main style="padding: 10px">
+        <nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// import adminMenu from '@/components/admin/adminMenu';
+
+export default {
+  // components: { adminMenu },
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.admin-layout-wrap {
+  // width: 100%;
+  height: 100vh;
+}
+</style>
