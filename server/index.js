919828503595b0b4184b275a48fee84549e5e685
Naya <naya252naya@gmail.com>
2023-06-03 15:23:41 +0300

    add api

--- server/index.js
+++ server/index.js
@@ -11,6 +11,15 @@ app.use(cors());
 const questions = require('./routes/api/questions');
 app.use('/api/questions', questions);
 
+//Handle production
+if(process.env.NODE_ENV === 'production'){
+    //Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + 'public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`) );
